3387b2db6cc91b18d7db561d93969464e8d4243c
Luciii0372 <143450514+Luciii0372@users.noreply.github.com>
2024-02-06 02:27:32 -0500

    warmUP passed

--- exer4/warmUp.js
+++ exer4/warmUp.js
@@ -16,7 +16,20 @@
  * F = anything < 60
  */
 function toLetterGrade(numGrade) {
-  // TODO
+  if (numGrade < 0 || numGrade > 100) {
+    return "INVALID"
+  }
+  if (numGrade >= 90) {
+    return "A"
+  } else if (numGrade >= 80) {
+    return "B"
+  } else if (numGrade >= 70) {
+    return "C"
+  } else if (numGrade >= 60) {
+    return "D"
+  } else {
+    return "F"
+  }
 }
 
 /**
@@ -27,7 +40,7 @@ function toLetterGrade(numGrade) {
  * Example: [9, 3, 4, 1, 2, 0] --> [4, 2, 0]
  */
 function getEvenElements(array) {
-  // TODO
+  return array.filter(element => element % 2 === 0)
 }
 
 /**
@@ -40,7 +53,15 @@ function getEvenElements(array) {
  * Example: "I love Bits of Good" --> 4
  */
 function findLongestWord(string) {
-  // TODO
+  let wordsList = string.split(" ")
+  let maxLen = 0
+  wordsList.forEach(element => {
+    let currLen = element.length
+    if (currLen > maxLen) {
+      maxLen = currLen
+    }
+  });
+  return maxLen
 }
 
 /**
@@ -65,7 +86,10 @@ function findLongestWord(string) {
     } 
  */
 function combineObjects(object1, object2) {
-  // TODO
+ return {
+    ... object1,
+    ... object2
+  }
 }
 
 /**
@@ -77,7 +101,11 @@ function combineObjects(object1, object2) {
  *  Example: [1, 2, 3] -> [3, 2, 1]
  */
 function reverseArr(array) {
-  // TODO
+  let finalArr = []
+  for (let i = array.length - 1; i >= 0; i--) {
+    finalArr.push(array[i])
+  }
+  return finalArr;
 }
 
 // DO NOT EDIT BELOW THIS LINE -- the code is for testing purposes only!
