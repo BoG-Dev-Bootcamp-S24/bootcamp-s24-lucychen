32d01606d5a83983deda567efed9fc951a422aa3
Luciii0372 <143450514+Luciii0372@users.noreply.github.com>
2024-02-06 23:52:35 -0500

    add changes

--- demo/lecture4/async_demo/practice.js
+++ demo/lecture4/async_demo/practice.js
@@ -8,7 +8,25 @@ document.getElementById('nameForm').addEventListener('submit', (event) => {
     const agifyUrl = `https://api.agify.io?name=${name}`;
     const genderizeUrl = `https://api.genderize.io?name=${name}`;
     const nationalizeUrl = `https://api.nationalize.io?name=${name}`;
+    fetchData(agifyUrl, genderizeUrl, nationalizeUrl);
+    
+    async function fetchData(agifyUrl, genderizeUrl, nationalizeUrl) {
+        try {
+            const responses = await Promise.all([
+                fetch(agifyUrl),
+                fetch(genderizeUrl),
+                fetch(nationalizeUrl)
+            ]);
 
+            document.getElementById('age').textContent = `Age: ${data[0].age}`;
+            document.getElementById('gender').textContent = `Gender: ${data[1].gender}`;
+            document.getElementById('nationality').textContent = `Nationality: ${data[2].country[0].country_id}`;
+    
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    }
+    
     // Students will write async code here to fetch data from the APIs
     // and update the DOM with the results.
     
