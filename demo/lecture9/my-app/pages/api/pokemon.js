69e86e93f0b5dbd59ad4dab364e640e8d4c568fd
Luciii0372 <143450514+Luciii0372@users.noreply.github.com>
2024-03-05 18:20:46 -0500

    Update pokemon.js

--- demo/lecture9/my-app/pages/api/pokemon.js
+++ demo/lecture9/my-app/pages/api/pokemon.js
@@ -1,33 +1,6 @@
 const pokemonData = {}
 
 export default async function handler(req, res) {
-<<<<<<< HEAD
-    const name = req.query.name;
-    const url = "https://pokeapi.co/api/v2/pokemon/";
-
-    if (req.method === 'GET') {
-        if (name) {
-            const callURL = url + name;
-            const response = await fetch(callURL);
-            const data = await response.json();
-            res.status(200).json(data);
-        }
-        if (pokemonData(name)) {
-            res.status(200).json(pokemonData(name));
-        }
-        if (name) {
-            const callURL = url + name;
-            const response = await fetch(callURL);
-            const data = await response.json();
-            res.status(200).json(data);
-        } else if (req.method === 'POST') {
-            const data = req.query.data;
-            pokemonData[name] = data;
-            res.status(200).json({message: "data overwritten for ${name}"});
-        }
-    }
-}
-=======
   const name = req.query.name;
   const url = "https://pokeapi.co/api/v2/pokemon/";
 
@@ -50,4 +23,3 @@ export default async function handler(req, res) {
     res.status(200).json({ message: `data overwritten for ${name}`});
   }
 }
->>>>>>> 8e8b51a1988a0fdbce862517e2501a20a1d9ad63
